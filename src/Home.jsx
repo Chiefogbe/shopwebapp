c561aef4f46c0f10db4ebd205c3ba7fc2214e116
Chiefogbe <watchmanjesse5002@gmail.com>
2023-10-31 08:55:07 +0100

    update name

--- src/Home.jsx
+++ src/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
     <div>
       
       <div className='HomePage'>
-      <h2>Best Jewellery Collection</h2>
+      <h2>Best Jewellery Collection Hub</h2>
       <p>It is a long established fact that a reader will be distracted by the readable content of a page when lokking at its layout. The point is using the lorem</p>
       <button>SHOP NOW</button>
       </div>
